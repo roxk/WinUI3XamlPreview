58d61de20369cff9d876cffcb76d066238b2c7ab
Roxk <vzox.rock@gmail.com>
2024-03-29 07:16:21 +0800

    ci-cd: Fix build

--- WinUI3XamlPreview/scripts/build-nuget.ps1
+++ WinUI3XamlPreview/scripts/build-nuget.ps1
@@ -7,9 +7,10 @@ $template = Get-Content $PSScriptRoot\..\Nuget\WinUI3XamlPreview.nuspec.template
 $nuspec = $template.Replace("VERSION", $version)
 Set-Content -path $PSScriptRoot\..\Nuget\.nuspec -value $nuspec
 . "$PSScriptRoot\get-msbuild"
+nuget restore $PSScriptRoot\..\WinUI3XamlPreviewCs\WinUI3XamlPreviewCs.csproj
 nuget restore $PSScriptRoot\..\WinUI3XamlPreview\WinUI3XamlPreview.vcxproj -PackagesDirectory $PSScriptRoot\..\packages
 &$msbuild $PSScriptRoot\.. -t:WinUI3XamlPreview -p:Platform=x64,Configuration=Release
-&$msbuild $PSScriptRoot\.. -t:WinUI3XamlPreviewCs -p:Platform=x64,Configuration=Release
+&$msbuild $PSScriptRoot\.. -t:WinUI3XamlPreviewCs -p:Platform="Any CPU" -p:Configuration=Release
 # TODO: Fix ARM support. idlgen is attempting to use ARM64/idlgen.exe when the host is x64.
 #&$msbuild $PSScriptRoot\..\WinUI3XamlPreview\WinUI3XamlPreview.vcxproj -p:Platform=ARM64,Configuration=Release
 nuget pack $PSScriptRoot\..\Nuget\.nuspec -outputDirectory $PSScriptRoot\..\Nuget
