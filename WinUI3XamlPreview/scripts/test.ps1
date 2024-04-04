d3ceb0dd33414d7db6d7dd595cecd231344b6fe3
Roxk <vzox.rock@gmail.com>
2024-04-05 00:55:57 +0800

    ci-cd: Test dll loader in CI

--- WinUI3XamlPreview/scripts/test.ps1
+++ WinUI3XamlPreview/scripts/test.ps1
@@ -4,4 +4,6 @@ $ErrorActionPreference = "Stop"
 nuget restore $PSScriptRoot\..\TestDep\TestDep.vcxproj -PackagesDirectory $PSScriptRoot\..\packages
 nuget restore $PSScriptRoot\..\WinUI3XamlPreview\WinUI3XamlPreview.vcxproj -PackagesDirectory $PSScriptRoot\..\packages
 nuget restore $PSScriptRoot\..\TestPackaged\TestPackaged.vcxproj -PackagesDirectory $PSScriptRoot\..\packages
+nuget restore $PSScriptRoot\..\WinUI3XamlPreview.DllLoader\WinUI3XamlPreview.DllLoader.vcxproj -PackagesDirectory $PSScriptRoot\..\packages
 &$msbuild $PSScriptRoot\.. -t:TestPackaged -p:Platform=x64,Configuration=Debug
+&$msbuild $PSScriptRoot\.. -t:WinUI3XamlPreview_DllLoader -p:Platform=x64,Configuration=Debug
